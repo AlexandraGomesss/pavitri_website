25cd72c696b80ab10b530b63d97b2f58f851ddd4
Alexandre Garcia <alexandregarcia@Macmini.lan>
2025-07-29 11:28:03 +0100

    custom colors added

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-screen">
+    <div className="relative w-full h-screen ">
       {/* Hero Banner */}
       <Image
         src={hero_banner}
@@ -16,9 +16,11 @@ export default function Home() {
 
       <NavBar />
       {/* Content on top of the banner */}
-      <div className="relative z-10 flex flex-col items-left justify-center h-full text-white bg-black/20 p-36">
+      <div className="relative z-10 flex flex-col items-left justify-center h-full text-light_grey bg-black/20 p-36">
         <h5 className="text-xl sm:text-4xl font-bold mb-4">Conecta-te com a</h5>
-        <h1 className="text-8xl sm:text-8xl mb-6 text-pink-200">RESPIRAÇÃO</h1>
+        <h1 className="text-8xl sm:text-8xl mb-6 text-light_pink">
+          RESPIRAÇÃO
+        </h1>
       </div>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // se usares o app router
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vibrant_green: "#C9FBBC",
+        light_green: "#D9E6D2",
+        dark_green: "#73986C",
+        light_pink: "#F2B7BC",
+        light_grey: "#F9F9F9",
+        dark_grey: "#2E2E2E",
+      },
+    },
+  },
+  plugins: [],
+};
